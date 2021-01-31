99669afcbba65397394226cfaf769007cfdec100
AshwiraBhaskar <bhaskarashwira55@gmail.com>
2021-01-31 20:53:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="w3-container">
+      
+      
+      <div class="w3-center">
+        <h1 style={{ fontWeight: 'bold' }} >Travel & Tourism Co. </h1>
+        </div>
+      
+      <div class="w3-panel w3-pale-red">
+        <p> hello </p>
+      </div>
+      
+      <div class="w3-center" >
+        <h4 style={{ fontWeight: 'bold' }}> Pick the place you want to visit </h4>
+        <div class = "w3-margin-top" ><button style={ {backgroundColor: 'orange'}}>Srinagar</button></div> 
+        <div class = "w3-margin-top"><button style={{border: 'none'},{backgroundColor: 'orange'}}>Jaisalmer</button></div> 
+        <div class = "w3-margin-top"><button style={{border: 'none'},{backgroundColor: 'orange'}}>Ooty</button></div> 
+        <div class = "w3-margin-top"><button style={{border: 'none'},{backgroundColor: 'orange'}}>Sikkim</button></div> 
+      </div>
+
     </div>
+    
   );
 }
 
